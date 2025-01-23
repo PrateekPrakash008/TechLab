c62ce32e4ffabdbd6c7b268904654858aecf638e
PrateekPrakkash <107049063+PrateekPrakkash@users.noreply.github.com>
2025-01-23 15:06:02 +0530

    Register provider Scripts

--- powershell/provider_resgiatration.ps1
+++ powershell/provider_resgiatration.ps1
@@ -1,3 +1,15 @@
+param (
+        [Parameter(Mandatory = $True)] [System.Guid] $CLIENT_ID,
+        [Parameter(Mandatory = $True)] [String] $CLIENT_SECRET,
+        [Parameter(Mandatory = $True)] [System.Guid] $TENANT_ID,
+        [Parameter(Mandatory = $True)] [String] $SUBSCRIPTION_ID
+    )
+
+$SecureStringPwd = $CLIENT_SECRET | ConvertTo-SecureString -AsPlainText -Force
+$Credential = New-Object -TypeName "System.Management.Automation.PSCredential" -ArgumentList $CLIENT_ID, $SecureStringPwd
+
+Connect-AzAccount -ServicePrincipal -TenantId $TENANT_ID -Credential $Credential -WarningAction SilentlyContinue -SubscriptionId $SUBSCRIPTION_ID | Out-Null
+
 $providerNamespaces = @(
     "Microsoft.SqlVirtualMachine"
 )
