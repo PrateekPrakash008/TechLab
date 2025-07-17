3f7974263578ab94c6be32491c62e92f77198949
PrateekPrakkash <107049063+PrateekPrakkash@users.noreply.github.com>
2025-07-17 19:39:04 +0530

    PIM via Terraform

--- data.tf
+++
@@ -1,7 +0,0 @@
-data "azurerm_management_group" "parent" {
-  name = local.geo["${var.GEO}_${var.ARCH_TYPE}"].parent
-}
-
-data "azuread_user" "user" {
-  user_principal_name = "${var.SUB_OWNER}@volvocars.com"
-}
